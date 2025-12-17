cb85d102ed2413a3345a30eafc5941b130202a16
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2025-12-17 13:14:33 +0530

    Added Date Indicator in Chat

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -47,43 +47,85 @@ function Messages({
 	}, [displayMessages.length, selectedUser]);
 
 	return (
-		<div className="flex-1 scroll-auto px-4 md:px-10 py-4 flex flex-col gap-2 overflow-auto relative">
+		<div
+			className={`flex-1 scroll-auto px-4 md:px-10 py-4 flex flex-col gap-2 overflow-auto relative ${
+				loading ? "justify-center items-center" : ""
+			}`}
+		>
 			{loading && (
-				<div className="text-center text-xs text-gray-400 py-2">
+				<div className="text-center text-xs font-semibold text-gray-400 py-2">
 					Loading history...
 				</div>
 			)}
 
 			{displayMessages.map((m, index) => {
 				const isMe = m.senderId === "me";
+				const prev =
+					index > 0
+						? new Date(
+								displayMessages[index - 1].timestamp
+						  ).toDateString()
+						: null;
+				const todayDate = new Date();
+				const yesterday = new Date(
+					todayDate.getDate() - 1
+				).toDateString();
+				const today = todayDate.toDateString();
+				const currDate = new Date(m.timestamp);
+				const curr = currDate.toDateString();
+				const formattedDate = currDate.toLocaleDateString([], {
+					day: "numeric",
+					month: "short",
+					year:
+						currDate.getFullYear() !== todayDate.getFullYear()
+							? "numeric"
+							: undefined,
+				});
+				let flag = false;
+				if (prev !== curr) {
+					flag = true;
+				}
+
 				return (
-					<div
-						key={index}
-						className={`w-full flex text-wrap ${
-							isMe ? "justify-end" : "justify-start"
-						}`}
-					>
+					<>
+						{flag && (
+							<div className="flex justify-center">
+								<div className="text-xs bg-gray-100 border border-gray-200 px-2 py-0.5 rounded-2xl text-gray-700">
+									{yesterday == curr
+										? "Yesterday"
+										: today == curr
+										? "Today"
+										: formattedDate}
+								</div>
+							</div>
+						)}
 						<div
-							className={`py-3 px-5 ${
-								isMe
-									? "bg-blue-500 rounded-br-none"
-									: "bg-blue-800 rounded-bl-none"
-							} text-white rounded-xl text-wrap wrap-break-word whitespace-pre-wrap max-w-[85%] md:max-w-[45%]`}
+							key={index}
+							className={`w-full flex text-wrap ${
+								isMe ? "justify-end" : "justify-start"
+							}`}
 						>
-							<p>{m.content}</p>
-							<div className="flex justify-end pt-1">
-								<span className="text-xs opacity-70">
-									{new Date(m.timestamp).toLocaleTimeString(
-										[],
-										{
+							<div
+								className={`py-3 px-5 ${
+									isMe
+										? "bg-blue-500 rounded-br-none"
+										: "bg-blue-800 rounded-bl-none"
+								} text-white rounded-xl text-wrap wrap-break-word whitespace-pre-wrap max-w-[85%] md:max-w-[45%]`}
+							>
+								<p>{m.content}</p>
+								<div className="flex justify-end pt-1">
+									<span className="text-xs opacity-70">
+										{new Date(
+											m.timestamp
+										).toLocaleTimeString([], {
 											hour: "2-digit",
 											minute: "2-digit",
-										}
-									)}
-								</span>
+										})}
+									</span>
+								</div>
 							</div>
 						</div>
-					</div>
+					</>
 				);
 			})}
 			<div ref={endRef} />
